9e778cb5bd3f15f2e41a2139372c675bce79a66f
DaffaAdityaDev <daffaaditya990@gmail.com>
2022-09-27 00:15:21 +0700

    first commit

---
+++ components/Post.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+function Post({ post }) {
+  return (
+    <div className='card'>
+      <img src={post.frontmatter.cover_image} alt='image'/>
+      <div className='post-date'>Posted on {post.frontmatter.date}</div>
+      <h3>{post.frontmatter.title}</h3>
+      <p>{post.frontmatter.excerpt}</p>
+      <Link href={`/blog/${post.slug}`}>
+        <a className='btn'>Read More</a>
+      </Link>
+    </div>
+  )
+}
+
+export default Post
